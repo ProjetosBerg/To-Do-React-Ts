558d6f348590149b56b38599c50b4970a2fd6b40
Bergkley <bergkley@gmail.com>
2024-11-01 19:34:50 -0300

    feat: to do react ts

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+// interface
+import { ITask } from "./interfaces/Task";
+import Modal from "./components/Modal";
+
+function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
+  const hideOrShowTask = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  }
+
+  const editTask = (task: ITask):void => {
+    hideOrShowTask(true);
+    setTaskToUpdate(task);
+  }
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowTask(false);
+
+  };
+
+  return (
+    <>
+      <div>
+        <Modal children= {<TaskForm btnText="Atualizar Tarefa" taskList={taskList} setTaskList={setTaskList} task={taskToUpdate} handleUpdate={updateTask}/> } title="Atualizar Tarefa"/>
+        <Header />
+        <main className={styles.main}>
+          <div>
+            <h2>O que você vai fazer?</h2>
+            <TaskForm btnText="Criar Tarefa" taskList={taskList} setTaskList={setTaskList} 
+             />
+          </div>
+          <div>
+            <h2>Suas tarefas</h2>
+            <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask} />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
